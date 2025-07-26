80f39b27346c61bd0f764597633dbd747376d8fc
Uriel Quiroz <uriel.quiroz@arrendadoradatamovil.com>
2025-07-26 12:02:05 -0600

    solucionando tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <main className="">
+      <main className="bg-red-500">
         <h1>Hola mundo</h1>
       </main>
   );
