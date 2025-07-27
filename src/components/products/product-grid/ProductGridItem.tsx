bbebd11d2f7b1547ba23e82d2a4c03b61ed82c0b
Uriel Quiroz <uriel.quiroz@arrendadoradatamovil.com>
2025-07-26 18:18:23 -0600

    Cambiar imagen - mouse hover

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,6 +1,8 @@
+'use client';
 import { Product } from "@/interfaces"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react";
 
 
 interface Props {
@@ -8,15 +10,18 @@ interface Props {
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+    const [displayimage, setDisplayImage] = useState(product.images[0])
   return (
     <div className="rounded-md overflow-hidden fade-in">
         <Link href={`/product/${product.slug}`}>
                 <Image
-                    src={`/products/${product.images[0]}`}
+                    src={`/products/${displayimage}`}
                     alt={product.title}
-                    className="w-full object-cover"
+                    className="w-full object-cover rounded"
                     width={500}
-                    height={500}/>
+                    height={500}
+                    onMouseEnter={() => setDisplayImage(product.images[1])}
+                    onMouseLeave={() => setDisplayImage(product.images[0])}/>
         </Link>
         
         <div className="p-4 flex flex-col">
