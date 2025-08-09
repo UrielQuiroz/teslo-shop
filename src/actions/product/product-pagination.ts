eb6d148a559aeb39f00ec7a6981eb4902f9d7534
Uriel Quiroz <uriel.quiroz@arrendadoradatamovil.com>
2025-08-09 13:12:40 -0600

    Mostrar productos en pantalla

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -5,6 +5,7 @@ import prisma from "@/lib/prisma"
 export const getPaginatedProductsWithImages = async () => {
     try {
         const products = await prisma.product.findMany({
+            take: 10,
             include: {
                 ProductImage: {
                     take: 2,
@@ -15,8 +16,15 @@ export const getPaginatedProductsWithImages = async () => {
             }
         })
 
-        console.log(products)
+        return {
+            currentPage: 1,
+            totalPages: 10,
+            products: products.map(product => ({
+                ...product,
+                images: product.ProductImage.map(image => image.url)
+            }))
+        }
     } catch (error) {
-        
+        throw new Error('No se pudo cargar los poductos');
     }
 }
