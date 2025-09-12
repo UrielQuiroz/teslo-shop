4407a5776332f6f5e76c6278d1aca3383b767cff
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-09-12 12:11:41 -0600

    Carrito - Hacer persistente el carrito de compras

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,5 +1,6 @@
 import { CartProduct } from "@/interfaces";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 
 interface State {
@@ -11,38 +12,44 @@ interface State {
 }
 
 export const useCartStore = create<State>()(
-    (set, get) => ({
-        cart: [],
+    persist(
+        (set, get) => ({
+            cart: [],
 
-        //Methods
-        addProductToCart: (product: CartProduct) => {
-            const { cart } = get();
-            console.log(cart)
+            //Methods
+            addProductToCart: (product: CartProduct) => {
+                const { cart } = get();
+                console.log(cart)
 
-            //1. Revisar si el producto existe en el carrito con la talla seleccionada
-            const productInCart = cart.some(item => item.id === product.id && item.size === product.size);
-            if( !productInCart ) {
-                set({
-                    cart: [
-                        ...cart,
-                        product
-                    ]
-                })
-                return;
-            }
-
-            //2. Se que el producto existe por talla... tengo que incrementar
-            const updateCartProducts = cart.map(item => {
-                if(item.id === product.id && item.size === product.size) {
-                    return {
-                        ...item,
-                        quantity: item.quantity + product.quantity
-                    }
+                //1. Revisar si el producto existe en el carrito con la talla seleccionada
+                const productInCart = cart.some(item => item.id === product.id && item.size === product.size);
+                if( !productInCart ) {
+                    set({
+                        cart: [
+                            ...cart,
+                            product
+                        ]
+                    })
+                    return;
                 }
-                return item;
-            })
 
-            set({ cart: updateCartProducts })
+                //2. Se que el producto existe por talla... tengo que incrementar
+                const updateCartProducts = cart.map(item => {
+                    if(item.id === product.id && item.size === product.size) {
+                        return {
+                            ...item,
+                            quantity: item.quantity + product.quantity
+                        }
+                    }
+                    return item;
+                })
+
+                set({ cart: updateCartProducts })
+            }
+        })
+
+        , {
+            name: 'shopping-cart'
         }
-    }
-))
+    )
+)
