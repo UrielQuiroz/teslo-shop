2083e8e2f91f4969c0dcac66de90357a9c66c91f
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-09-16 22:45:08 -0600

    Validar usuario - NextAuth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,8 @@
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import z from "zod";
+import prisma from "./lib/prisma";
+import bcrypt from "bcryptjs";
 
 export const authConfig: NextAuthConfig = {
     pages: {
@@ -22,12 +24,19 @@ export const authConfig: NextAuthConfig = {
                     console.log({ email, password })
 
                     //Buscar el correo
-
+                    const user = await prisma.user.findUnique({
+                        where: { email: email.toLowerCase() }
+                    })
+                    if( !user ) return null;
+                    
                     //Comparar las contraseñas
+                    if( !bcrypt.compareSync( password, user.password )) return null;                    
 
-                    //Regresar al usuario
+                    //Regresar al usuario sin el password
+                    const { password: _, ...rest } = user;
 
-                return null
+                    console.log({ rest })
+                    return rest;
             },
         })
     ]
