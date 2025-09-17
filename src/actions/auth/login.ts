cc2a99d51de8d0c3cf09e593b39fedfca43a2f55
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-09-16 21:56:46 -0600

    Conectar Login Form con NextAuth

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { signIn } from "@/auth.config"
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData
+) {
+    try {
+        
+        console.log({ formData: Object.fromEntries(formData) });
+
+        await signIn('credentials', Object.fromEntries(formData));
+
+    } catch (error) {
+        return 'CredentialsSignin'
+    }
+}
