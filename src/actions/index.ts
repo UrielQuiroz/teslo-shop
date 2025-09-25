173bbb5b7a0846891f2965b049f2de3111ec6911
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-09-25 13:33:02 -0600

    Server Action - Verificar Pago - obteniendo token de paypal

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -13,6 +13,7 @@ export * from './order/get-order-by-id'
 export * from './order/get-orders-by-user' 
 
 export * from './payments/set-transaction-id'
+export * from './payments/paypal-check-payment'
 
 export * from './auth/login'
 export * from './auth/logout'
--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
-import { CreateOrderData, CreateOrderActions } from "@paypal/paypal-js"
-import { setTransactionId } from "@/actions";
+import { CreateOrderData, CreateOrderActions, OnApproveData, OnApproveActions } from "@paypal/paypal-js"
+import { paypalCheckPayment, setTransactionId } from "@/actions";
 
 interface Props {
   orderId: string;
@@ -47,10 +47,19 @@ export const PayPalButton = ({ orderId, amount}: Props) => {
     return transactionId
   }
 
+  const onApprove = async (data: OnApproveData, actions: OnApproveActions) => {
+    console.log('onApprove')
+    const details = await actions.order?.capture();
+    console.log(details)
+    if( !details ) return;
+
+    await paypalCheckPayment( details.id ?? '' );
+  }
+
   return (
     <PayPalButtons
       createOrder={createOrder}
-      // onApprove={}
+      onApprove={onApprove}
       />
   )
 }
