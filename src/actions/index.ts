08d2a46abac07ce1760bbb2bf5d6b7bebd432c6c
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-09-27 17:27:05 -0600

    Server Action - Esquema de validación de objetos

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 export * from './product/product-pagination'
 export * from './product/get-product-by-slug'
 export * from './product/get-stock-by-slug'
+export * from './product/create-update-product'
 
 export * from './country/get-countries'
 
